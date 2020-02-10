4c70ed4d46ce684509841965100067b0e47a3f60
Patrick Mueller <pmuellr@gmail.com>
2020-02-10 10:40:02 -0500

    enhance to support three hosts

--- README.md
+++ README.md
@@ -21,15 +21,34 @@ example
 ================================================================================
 
 ```console
-$ es-apm-sys-sim 1 apm-sys-sim example.com https://elastic:changeme@localhost:9200
-help: press a/s to modify cpu, d/f to modify mem, "q" to exit
-current data written: hostname: example cpu: 0.4 mem: 400000
-    (key "a" pressed)
-current data written: hostname: example cpu: 0.2 mem: 400000
-    (key "a" pressed again)
-current data written: hostname: example cpu: 0 mem: 400000
-    (30 seconds later)
-total docs written: 29
+$ es-apm-sys-sim 1 apm-sys-sim https://elastic:changeme@localhost:9200
+
+----------------------------------------------------
+help: press "q" to exit, "h" for help
+  host-A: press q/w to modify cpu, e/r to modify mem
+  host-B: press a/s to modify cpu, d/f to modify mem
+  host-C: press z/x to modify cpu, c/v to modify mem
+
+current data written:
+  host-A cpu:  0.4 free mem: 400KB
+  host-B cpu:  0.4 free mem: 400KB
+  host-C cpu:  0.4 free mem: 400KB
+----------------------------------------------------
+                                 (key q pressed now)
+current data written:
+  host-A cpu:  0.3 free mem: 400KB
+  host-B cpu:  0.4 free mem: 400KB
+  host-C cpu:  0.4 free mem: 400KB
+                                 (key q pressed now)
+current data written:
+  host-A cpu:  0.2 free mem: 400KB
+  host-B cpu:  0.4 free mem: 400KB
+  host-C cpu:  0.4 free mem: 400KB
+                                 (key q pressed now)
+current data written:
+  host-A cpu:  0.1 free mem: 400KB
+  host-B cpu:  0.4 free mem: 400KB
+  host-C cpu:  0.4 free mem: 400KB
 ...
 ```
 
@@ -38,20 +57,17 @@ usage
 ================================================================================
 
 ```
-es-apm-sys-sim <interval> <index-name> <host-name> <elastic-search-url>
+es-apm-sys-sim <interval> <index-name> <elastic-search-url>
 ```
 
-Every `<interval>` seconds, a document will be written to `<index-name>` at
-the elasticsearch cluster `<elastic-search-url>` using the specified
-`<host-name>`.
+Every `<interval>` seconds, documents will be written to `<index-name>` at
+the elasticsearch cluster `<elastic-search-url>` using host names
+`host-A`, `host-B`, and `host-C`.
 
 You can quit the program by pressing `q` or `control-c`.  
 
-You can change the `system.cpu.total.norm.pct` value written by pressing
-`a` to decrease and `s` to increase.
-
-You can change the `system.memory.actual.free` value written by pressing
-`d` to decrease and `f` to increase.
+You can change the `system.cpu.total.norm.pct` and ``system.memory.actual.free`
+values written by pressing keys listed in the console output.
 
 Every 30 seconds, the number of documents indexed is logged.
 
@@ -62,7 +78,7 @@ more fields.
 {
     @timestamp: '2019-12-15T17:16:44.765Z',
     host: {
-        name: 'from-parameter'
+        name: 'host-A'
     },
     system: {
         cpu: {
--- es-apm-sys-sim.js
+++ es-apm-sys-sim.js
@@ -25,13 +25,12 @@ if (cliOptions.flags.help || cliOptions.input.length === 0) {
   process.exit(1)
 }
 
-const [intervalS, indexName, hostName, clusterURL] = cliOptions.input
+const [intervalS, indexName, clusterURL] = cliOptions.input
 
 if (intervalS == null) logError('intervalSeconds parameter missing')
 const interval = parseInt(intervalS, 10)
 if (isNaN(interval)) logError(`invalid interval parameter: ${intervalS}`)
 if (indexName == null) logError('indexName parameter missing')
-if (hostName == null) logError('hostName parameter missing')
 if (clusterURL == null) logError('clusterURL parameter missing')
 
 let esClient
@@ -48,33 +47,66 @@ try {
 }
 
 let DocsWritten = 0
-const CpuMetric = createMetric(0.4, 0.2, 0, 1)
-const MemMetric = createMetric(400 * 1000, 100 * 1000, 0, 400 * 1000)
+const CpuMetrics = {
+  A: createMetric(0.4, 0.1, 0, 1),
+  B: createMetric(0.4, 0.1, 0, 1),
+  C: createMetric(0.4, 0.1, 0, 1)
+}
+
+const MemMetrics = {
+  A: createMetric(400 * 1000, 100 * 1000, 0, 900 * 1000),
+  B: createMetric(400 * 1000, 100 * 1000, 0, 900 * 1000),
+  C: createMetric(400 * 1000, 100 * 1000, 0, 900 * 1000)
+}
 
 printRuntimeHelp()
 
-setInterval(writeDoc, interval * 1000)
+setInterval(writeDocs, interval * 1000)
 setInterval(logDocsWritten, 30 * 1000)
 
 kbd.on(null, printRuntimeHelp)
+kbd.on('h', printFullHelp)
 kbd.on('c-c', () => process.exit())
-kbd.on('q', () => process.exit())
+kbd.on('s-q', () => process.exit())
 
-kbd.on('a', () => { CpuMetric.down(); printCurrent() })
-kbd.on('s', () => { CpuMetric.up(); printCurrent() })
-kbd.on('d', () => { MemMetric.down(); printCurrent() })
-kbd.on('f', () => { MemMetric.up(); printCurrent() })
+kbd.on('q', () => { CpuMetrics.A.down(); printCurrent() })
+kbd.on('w', () => { CpuMetrics.A.up(); printCurrent() })
+kbd.on('e', () => { MemMetrics.A.down(); printCurrent() })
+kbd.on('r', () => { MemMetrics.A.up(); printCurrent() })
+
+kbd.on('a', () => { CpuMetrics.B.down(); printCurrent() })
+kbd.on('s', () => { CpuMetrics.B.up(); printCurrent() })
+kbd.on('d', () => { MemMetrics.B.down(); printCurrent() })
+kbd.on('f', () => { MemMetrics.B.up(); printCurrent() })
+
+kbd.on('z', () => { CpuMetrics.C.down(); printCurrent() })
+kbd.on('x', () => { CpuMetrics.C.up(); printCurrent() })
+kbd.on('c', () => { MemMetrics.C.down(); printCurrent() })
+kbd.on('v', () => { MemMetrics.C.up(); printCurrent() })
 
 function printCurrent () {
-  console.log(`current data written: hostname: ${hostName} cpu: ${CpuMetric.currentValue} mem: ${MemMetric.currentValue}`)
+  console.log(`current data written:`);
+  for (const name of Object.keys(CpuMetrics)) {
+    const cpuMetric = CpuMetrics[name]
+    const memMetric = MemMetrics[name]
+    const cpu = `${cpuMetric.currentValue}`.padStart(4)
+    const mem = `${memMetric.currentValue / 1000}`.padStart(3)
+    console.log(`  host-${name} cpu: ${cpu} free mem: ${mem}KB`)
+  }
 }
 
 function logDocsWritten () {
   console.log(`total docs written: ${DocsWritten}`)
 }
 
-async function writeDoc () {
-  const doc = generateDoc()
+async function writeDocs () {
+  for (const name of Object.keys(CpuMetrics)) {
+    await writeDoc(name)
+  }
+}
+
+async function writeDoc (name) {
+  const doc = generateDoc(name)
 
   let response
   try {
@@ -83,38 +115,54 @@ async function writeDoc () {
       body: doc
     })
   } catch (err) {
-    logError(`error indexing document: ${err.message}`)
+    logError(`error indexing document: ${JSON.stringify(err, null, 4)}`)
   }
 
   if (response.statusCode !== 201) {
-    logError(`unexpected error indexing document: ${JSON.stringify(response)}`)
+    logError(`unexpected error indexing document: ${JSON.stringify(response, null, 4)}`)
   }
 
   DocsWritten++
 }
 
-function printRuntimeHelp () {
-  console.log('help: press a/s to modify cpu, d/f to modify mem, "q" to exit')
-  printCurrent()
+function printFullHelp () {
+  console.log('')
+  console.log('-------------------------------------------------------')
+  console.log(getHelp())
+  console.log('-------------------------------------------------------')
+  console.log('')
 }
 
-function generateDoc () {
+function printRuntimeHelp () {
+  console.log('')
+  console.log('-------------------------------------------------------')
+  console.log('help: press "shift-q" or "ctrl-c" to exit, "h" for help')
+  console.log('  host-A: press q/w to modify cpu, e/r to modify mem')
+  console.log('  host-B: press a/s to modify cpu, d/f to modify mem')
+  console.log('  host-C: press z/x to modify cpu, c/v to modify mem')
+  console.log()
+  printCurrent()
+  console.log('-------------------------------------------------------')
+  console.log('')
+}
+
+function generateDoc (name) {
   return {
     '@timestamp': new Date().toISOString(),
     host: {
-        name: hostName
+        name: `host-${name}`
     },
     system: {
         cpu: {
             total: {
                 norm: {
-                    pct: CpuMetric.currentValue
+                    pct: CpuMetrics[name].currentValue
                 }
             }
         },
         memory: {
             actual: {
-                free: MemMetric.currentValue
+                free: MemMetrics[name].currentValue
             },
             total: 1000000
         }
@@ -124,11 +172,19 @@ function generateDoc () {
 
 function getHelp () {
   return `
-es-apm-sys-sim <intervalSeconds> <indexName> <hostName> <clusterURL>
+es-apm-sys-sim <intervalSeconds> <indexName> <clusterURL>
 
 Writes apm system metrics documents on an interval, allowing the cpu usage and
 free mem metrics to be changed with keyboard presses.
-  `.trim()
+
+Fields in documents written:
+  @timestamp                 current time
+  host.name                  host name 
+  host.name.keyword          host name (keyword field for aggregations)
+  system.cpu.total.norm.pct  cpu usage,    0 -> 1 
+  memory.actual.free         free memory,  0 -> 900KB 
+  memory.total               total memory, 1MB heh
+`.trim()
 }
 
 function logError (message) {
